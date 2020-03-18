4516f716490ef41c72f4f885b6dabb4d67096523
Ayush Arora <aayusharora6896@gmail.com>
2020-03-18 02:02:21 -0400

    Third Commit

--- src/kpt_project_restaurant.java
+++ src/kpt_project_restaurant.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class kpt_project_restaurant {
     ArrayList<ReadCSV.Reviews> data1;
@@ -29,11 +30,20 @@ public class kpt_project_restaurant {
         String delimiter = ",";
         read_analysis_words a1 = new read_analysis_words();
         positive = a1.tokenize(delimiter, "positive.txt");
-        System.out.println(positive);
+        Arrays.sort(positive);
+        for (String word:positive) {
+            System.out.print(word  + " ");
+        }
         negative = a1.tokenize(delimiter, "negative.txt");
-        System.out.println(negative);
+        Arrays.sort(negative);
+        for (String word:negative) {
+            System.out.print(word  + " ");
+        }
         neutral = a1.tokenize(delimiter, "neutral.txt");
-        System.out.println(neutral);
+        Arrays.sort(neutral);
+        for (String word:neutral) {
+            System.out.print(word + " ");
+        }
     }
     public int isInPositive(String key) {
         int lo=0;
@@ -42,9 +52,9 @@ public class kpt_project_restaurant {
         while(lo<=hi) {
             int mid = lo +(hi-lo)/2;
             int result = key.compareTo(positive[mid]);
-            if(result<0) hi = mid-1;
-            else if(result > 0) lo = mid+1;
-            else return mid;
+            if(result<0) {hi = mid-1;}
+            else if(result > 0) {lo = mid+1;}
+            else {return mid;}
         }
         return -1;
     }
@@ -52,14 +62,13 @@ public class kpt_project_restaurant {
     public int isInNegative(String key) {
         int lo=0;
         int hi = negative.length-1;
-        // System.out.println(stopWords.length);
 
         while(lo<=hi) {
             int mid = lo +(hi-lo)/2;
             int result = key.compareTo(negative[mid]);
-            if(result<0) hi = mid-1;
-            else if(result > 0) lo = mid+1;
-            else return mid;
+            if(result<0) {hi = mid-1;}
+            else if(result > 0) {lo = mid+1;}
+            else {return mid;}
         }
         return -1;
     }
@@ -71,9 +80,9 @@ public class kpt_project_restaurant {
         while(lo<=hi) {
             int mid = lo +(hi-lo)/2;
             int result = key.compareTo(neutral[mid]);
-            if(result<0) hi = mid-1;
-            else if(result > 0) lo = mid+1;
-            else return mid;
+            if(result<0) {hi = mid-1;}
+            else if(result > 0) {lo = mid+1;}
+            else {return mid;}
         }
         return -1;
     }
@@ -83,38 +92,39 @@ public class kpt_project_restaurant {
         int negativeCount = 0;
         int neutralCount = 0;
         int netValue = 0;
-                    System.out.println(entry.restaurantID + " = " + entry.reviewArray);
-                    System.out.println("Size of array from calculateReviewRating " + entry.reviewArray.size());
                 for (String word:entry.reviewArray) {
-                    System.out.println("Word being checked = " + word);
                 if(isInPositive(word) != -1){
-                    System.out.println("+ word found " + word );
                     positiveCount = positiveCount + 1;
                 }
                 else if(isInNegative(word) != -1){
-                    System.out.println("- word found " + word );
                     negativeCount = negativeCount - 1;
                 }
                 else if(isInNeutral(word) != -1){
-                    System.out.println("neutral word found " + word );
                     neutralCount = neutralCount + 0;
                 }
                 else{
-                    System.out.println("Word not found");
                     continue;
                 }
             }
 
         netValue = positiveCount + negativeCount + neutralCount;
-        System.out.println(netValue);
         return netValue;
     }
 
     public void sentimentAnalysis(){
-        System.out.println("THis is the new pure");
         System.out.println(pure);
         for (Parse.pureReview entry:pure) {
-            calculateReviewRating(entry);
+            int value = 0;
+            value = calculateReviewRating(entry);
+            if(value == 0 ){
+                System.out.println("Neutral Review");
+            }
+            else if(value > 0){
+                System.out.println("Positive Review");
+            }
+            else {
+                System.out.println("Negative Review");
+            }
         }
     }
 
